3c3cafe89d981c0abe799e95dee7ff67a80086fb
George Davis IV <gdavisiv@gmail.com>
2020-08-14 16:43:24 -0400

    Initial Commit

---
+++ PhotoJournal/PhotoJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoJournalApp.swift
+//  PhotoJournal
+//
+//  Created by George Davis IV on 8/14/20.
+//
+
+import SwiftUI
+
+@main
+struct PhotoJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
