401acb3ede5c32a9f14d6370328fe19a5f622698
George Davis IV <gdavisiv@gmail.com>
2020-08-14 17:13:58 -0400

    Created Home struct

--- PhotoJournal/ContentView.swift
+++ PhotoJournal/ContentView.swift
@@ -9,12 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+        Home()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Home : View {
+    var body: some View{
+        ScrollView(.vertical, showsIndicators: true){
+            VStack{
+                HStack{
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        Image(systemName: "rectangle.grid.1x2")
+                            .renderingMode(.template)
+                            .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                    }
+                }
+            }
+        }
     }
 }
