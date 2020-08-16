7e88d6097fcffcac1c8ed9c4cdc768b489cb752c
George Davis IV <gdavisiv@gmail.com>
2020-08-16 17:27:43 -0400

    UI Updates: Buttons Added

--- PhotoJournal/ContentView.swift
+++ PhotoJournal/ContentView.swift
@@ -20,12 +20,15 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct Home : View {
+    
+    @State var index = 0
+    
     var body: some View{
         ScrollView(.vertical, showsIndicators: true){
             VStack{
                 HStack{
                     Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                        Image(systemName: "rectangle.grid.1x2")
+                        Image(systemName: "rectangle.3.offgrid")
                             .renderingMode(.template)
                             .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
                     }
@@ -47,12 +50,42 @@ struct Home : View {
                     Spacer(minLength: 0)
                     
                     Button(action: {}) {
-                        Image(systemName: "circle.grid.2x2")
+                        Image(systemName: "rectangle.grid.1x2")
                             .renderingMode(.template)
                             .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
                     }
                 }
                 .padding(.horizontal)
+                
+                HStack{
+                    Text("Animated")
+                        .font(.caption)
+                        .foregroundColor(index == 0 ? .white : Color("Color").opacity(0.85))
+                        .fontWeight(.bold)
+                        .padding(.vertical,6)
+                        .padding(.horizontal,20)
+                        .background(Color("Color1").opacity(index == 0 ? 1 : 0))
+                        .clipShape(Capsule())
+                        .onTapGesture {
+                            index = 0
+                        }
+                    
+                    Text("20+ Series")
+                        .font(.caption)
+                        .foregroundColor(index == 1 ? .white : Color("Color").opacity(0.85))
+                        .fontWeight(.bold)
+                        .padding(.vertical,6)
+                        .padding(.horizontal,20)
+                        .background(Color("Color1").opacity(index == 1 ? 1 : 0))
+                        .clipShape(Capsule())
+                        .onTapGesture {
+                            index = 1
+                        }
+                    
+                    Spacer()
+                }
+                .padding(.horizontal)
+                .padding(.top, 10)
                 
                 Spacer()
             }
